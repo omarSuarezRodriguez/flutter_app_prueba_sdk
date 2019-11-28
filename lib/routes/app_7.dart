6d0a8bf3501f30ef4397b8d74bd677a12b68e4ae
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-28 11:01:39 -0500

    commit

--- lib/routes/app_7.dart
+++ lib/routes/app_7.dart
@@ -3,15 +3,67 @@ import 'package:flutter/material.dart';
 class App_7 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Variables y constantes
+    String ejercicio = "Mostrar los números pares entre el 1 al 100";
+
+    //TODO el código
     return ListView(
+      padding: EdgeInsets.all(15),
       children: <Widget>[
-        Center(
-          child: Text("Prueba"),
+        FlatButton(
+          onPressed: () {
+            showMyDialog(context, ejercicio);
+          },
+          child: Text("App 7"),
         ),
-        Text("Prueba 7"),
-        Text("Prueba 2"),
-        Text("Prueba 3"),
+        RaisedButton(
+          onPressed: () {
+            eventoClickBoton(context);
+          },
+          child: Text("Mostrar"),
+        ),
       ],
     );
-  }
-}
+  } //Fin body app
+
+//Metodos
+
+//Metodo 1
+//Método eventoClickBoton
+  void eventoClickBoton(BuildContext context) {
+    String cadena = "Números pares del 1 al 100:\n";
+
+    for (int i = 1; i <= 100; i++) {
+      if (i%2 == 0) {
+        cadena += i.toString() + " ";
+      }
+    }
+
+    showMyDialog(context, cadena);
+
+  } //Fin método eventoClickBoton
+
+//Método 2
+//Método AlertDialog
+  void showMyDialog(BuildContext context, String texto) {
+    showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(
+            texto,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } //Fin método showMyDialog
+
+} //Fin class App_7
--- lib/routes/app_8.dart
+++ lib/routes/app_8.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class App_8 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('App 8', style: TextStyle(fontSize: 40.0)),
+    //Variables y Controladores
+    String ejercicio = "Juego de adivinar número:\n\nGeneramos un número del 1 "
+        "al 100, Tenemos que adivinar, si fallamos nos dirá si es mayor "
+        "o menor, también mostrar el número de intentos requeridos";
+    final miControlador_1 =
+        TextEditingController(); //Controlador de TextFormField
+    var rgn = new Random(); //Objeto random
+    int aleatorio = rgn.nextInt(100); //Número aleatorio
+    //int contador = 0; //Contador de intentos
+    final data = Data(counter: 0); //Objeto data con counter en 0
+
+    //TODO el código
+    return ListView(
+      padding: EdgeInsets.all(15),
+      children: <Widget>[
+        FlatButton(
+          onPressed: () {
+            showMyDialog(context, ejercicio);
+          },
+          child: Text("App 8"),
+        ),
+        TextFormField(
+          controller: miControlador_1,
+          decoration: InputDecoration(labelText: "Número"),
+        ),
+        RaisedButton(
+          onPressed: () {
+            eventoClickBoton(context, miControlador_1, aleatorio, data);
+          },
+          child: Text("Adivinar"),
+        ),
+      ],
     );
-  }
+  } //Fin body app
+
+//Métodos
+
+//Método 1
+//Método eventoClickBoton
+  void eventoClickBoton(BuildContext context,
+      TextEditingController miControlador_1, int aleatorio, Data data) {
+    int num = int.parse(miControlador_1.text);
+
+    //Comparo los valores
+    if (num == aleatorio) {
+      String texto = "Has ganado!, Nro Intentos:\n" +
+          data.counter.toString(); //Muestro con data.counter
+      showMyDialog(context, texto);
+    } else if (num > aleatorio) {
+      showMyDialog(context, "El número es menor\n\n" + aleatorio.toString());
+      data.counter += 1; //Le sumo 1 al contador
+    } else if (num < aleatorio) {
+      data.counter += 1; //Le sumo 1 al contador
+      showMyDialog(context, "El número es mayor\n\n" + aleatorio.toString());
+    }
+  } //Fin método eventoClickBoton
+
+//Método 2
+//Método AlertDialog
+  void showMyDialog(BuildContext context, String texto) {
+    showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(
+            texto,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } //Fin método showMyDialog
+
+} //Fin class App_8
+
+//Método 3
+//Data
+//Clase para objetos con data
+class Data {
+//  String text;
+  int counter;
+
+//  String dateTime;
+
+  Data({this.counter});
 }
